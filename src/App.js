b2df7abac5836c363da9fa94883977efaf683bbf
MoadBJ <moadboujmaa@gmail.com>
2022-09-18 10:44:13 +0100

    Create React App And Adding Header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Header/>
+    </div>
+  );
+}
