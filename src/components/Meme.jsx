caba6976a78f13d15a8aa99770abd6c7960ff8b4
MoadBJ <moadboujmaa@gmail.com>
2022-09-18 11:02:52 +0100

    Adding a Form to project

---
+++ src/components/Meme.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+    <main>
+        <form className="form">
+            <input 
+                type="text" 
+                className="form-input"
+                placeholder="Top Text"
+            />
+            <input 
+                type="text" 
+                className="form-input"
+                placeholder="Bottom Text"
+            />
+            <button className="form-button">Get A New Meme Image 🖼️</button>
+        </form>
+    </main>
+    )
+}
